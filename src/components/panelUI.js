e81de674ac2f9d52d0571477cfb9ea10c626b6a3
NastyPigz <williamzwy06701@gmail.com>
2021-09-03 15:30:35 -0400

    public

--- src/components/panelUI.js
+++ src/components/panelUI.js
@@ -1,13 +1,16 @@
 import Calendar from 'react-calendar';
 import {useState} from 'react';
+import db from './panelitems.json';
 
 export function PanelUI () {
     const [clicked, setClicked] = useState(false);
     const [classNames, setClassNames] = useState({
         main: "mainPanel",
         left: "leftSlide",
-        right: "rightSlide"
+        right: "rightSlide",
+        mid: "midSlide"
     });
+    const [current, setCurrent] = useState("reminder");
     function handleClick () {
         if (clicked) {
             setClicked(false);
@@ -15,7 +18,8 @@ export function PanelUI () {
             setClassNames({
                 main: "mainPanel",
                 left: "leftSlide",
-                right: "rightSlide"
+                right: "rightSlide",
+                mid: "midSlide"
             });
         } else {
             setClicked(true);
@@ -23,7 +27,8 @@ export function PanelUI () {
             setClassNames({
                 main: "mainPan",
                 left: "leftSlid",
-                right: "rightSlid"
+                right: "rightSlid",
+                mid: "midSlid"
             });
         }
     }
@@ -31,14 +36,44 @@ export function PanelUI () {
     <>
       <div className={classNames.main}>
         <div className={classNames.left}>
-          <img alt="mainIcon" src="https://pbs.twimg.com/profile_images/905183271046193153/q_P1KBUJ_400x400.jpg" />
-          <img alt="Icon1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK6kDX7VXFmTnAknkm7bzOoAofTcdQW7QKJ11i-ekF4uhIcLgE0k3O8v6p1p44her8an8&usqp=CAU" />
-          <img alt="Icon2" src="https://pbs.twimg.com/profile_images/905183271046193153/q_P1KBUJ_400x400.jpg" />
-          <img alt="Icon3" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK6kDX7VXFmTnAknkm7bzOoAofTcdQW7QKJ11i-ekF4uhIcLgE0k3O8v6p1p44her8an8&usqp=CAU" />
+          <img 
+            className="mainIcon"
+            alt="mainIcon"
+            src="https://pbs.twimg.com/profile_images/905183271046193153/q_P1KBUJ_400x400.jpg"
+          />
+          <img
+            alt="Icon1"
+            onClick={() => {
+              setCurrent("reminder");
+            }}
+            src="https://static.thenounproject.com/png/1604214-200.png" 
+            style={{backgroundColor: clicked ? 'transparent' : 'white'}}
+          />
+          <img 
+            alt="Icon2"
+            onClick={() => {
+              setCurrent("pharmacy");
+            }}
+            src="https://cdn.vox-cdn.com/thumbor/pOMbzDvdEWS8NIeUuhxp23wi_cU=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/19700731/googlemaps.png"
+          />
+          <img 
+            alt="Icon3"
+            onClick={() => {
+              setCurrent("map");
+            }}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK6kDX7VXFmTnAknkm7bzOoAofTcdQW7QKJ11i-ekF4uhIcLgE0k3O8v6p1p44her8an8&usqp=CAU"
+          />
+        </div>
+        <div className={classNames.mid}>
+          <div className="panelName">{db[current]}</div>
+          <ul>
+            {db.list[current].map((item) => <li>{item}</li>)}
+          </ul>
         </div>
         <div className={classNames.right}>
           <button className="modeToggle" onClick={handleClick} id="toggleBtn">Toggle</button>
-          <Calendar id="panelCalender" />
+          <Calendar id="panelCalender" calendarType="US" />
+          {/* <div>Test</div>
           <div>Test</div>
           <div>Test</div>
           <div>Test</div>
@@ -87,8 +122,7 @@ export function PanelUI () {
           <div>Test</div>
           <div>Test</div>
           <div>Test</div>
-          <div>Test</div>
-          <div>Test</div>
+          <div>Test</div> */}
         </div>
         {function() {
           if (localStorage !== undefined) {
